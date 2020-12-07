20ba906055ed116619c9234d18b0fc01be479905
manjari jain <manjarijain98@gmail.com>
2020-12-07 16:08:53 +0530

    first commit

--- app.py
+++ app.py
@@ -87,14 +87,15 @@ dp=Dispatcher(bot,None)
 dp.add_handler(CommandHandler("start",start))
 dp.add_handler(CommandHandler("help",_help))
 dp.add_handler(CommandHandler("news",news))
-    #dp.add_handler(MessageHandler(Filters.text,echo_text))
+#dp.add_handler(MessageHandler(Filters.text,echo_text))
 dp.add_handler(MessageHandler(Filters.sticker,echo_sticker))
 dp.add_handler(MessageHandler(Filters.text,reply_text))
 dp.add_error_handler(error)
+app.run(port=8443)
 
-if __name__=="__main__":
+
    
-    app.run(port=8443)
+  
 
 
 
